d3b89574aa066a377905a8e242db8ae35e125554
olsib8 <106234333+olsib8@users.noreply.github.com>
2023-06-15 18:30:35 +0000

    Deploying to gh-pages from @ olsib8/goit-react-hw-08-phonebook@41c6589e4f80e66d3f20c71a72f52d80d0771a21 🚀

--- static/js/725.f22d41b5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[725],{2725:function(e,n,t){t.r(n);var o=t(4565),c=t(184);n.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)(o.Z,{component:"p",variant:"h4",textAlign:"center",mt:24,children:"Page not found"})})}}}]);
-//# sourceMappingURL=725.f22d41b5.chunk.js.map
